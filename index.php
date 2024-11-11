3ee3534f3b2515b86e0c80f1d4a200328447af38
drakin9 <gui.olialves2@gmail.com>
2024-11-11 19:58:27 -0300

    1.0.3

---
+++ index.php
@@ -0,0 +1,28 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<meta charset="utf-8">
+	<link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
+	<link rel="stylesheet" type="text/css" href="css/estilo.css">
+	<meta name="viewport" content="width=device-width, initial-scale=1">
+	<title></title>
+</head>
+<body>
+	<header>
+		<img src="imagem/imagem.png" alt="logo">
+		<nav>
+			<ul>
+				<li><a href="#">Nome</a></li>
+				<li><a href="#">Produtos</a></li>
+				<li><a href="#">Fornecedores</a></li>
+				<li><a href="#">Categorias</a></li>
+			</ul>
+		</nav>
+	</header>
+	<main><h2>Conteúdo</h2></main>
+	<footer><h2>Rodapé</h2></footer>
+
+	
+	<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-YvpcrYf0tY3lHB60NNkmXc5s9fDVZLESaAA55NDzOxhy9GkcIdslK1eN7N6jIeHz" crossorigin="anonymous"></script> 
+</body>
+</html>
